b0cd17450aa998c05348753fde468b0a0be2e13b
yangjiang <jiangyang5157@gmail.com>
2017-07-21 20:50:58 +1200

    na

---
+++ BinaryGap/binary-gap.go
@@ -0,0 +1,41 @@
+//https://codility.com/programmers/lessons/1-iterations/binary_gap/
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	input := 51712
+	output := Solution(input)
+	fmt.Printf("Integer=%v\nBinaryGap=%v\n", strconv.FormatInt(int64(input), 2), output)
+}
+
+//Find longest sequence of zeros in binary representation of an integer.
+func Solution(N int) int {
+	ret := 0
+	count := -1
+	for N > 0 {
+		if N%2 == 0 {
+			if count != -1 {
+				count++
+			}
+		} else {
+			if count > 0 {
+				ret = MaxInt(ret, count)
+			}
+			count = 0
+		}
+		N = N >> 1
+	}
+	return ret
+}
+
+func MaxInt(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
