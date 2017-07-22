7a0a397cc32b5581a3c8b4b3b7a427b44a110a94
yangjiang <jiangyang5157@gmail.com>
2017-07-23 01:32:54 +1200

    na

--- NumberOfDiscIntersections/number-of-disc-intersections.go
+++ NumberOfDiscIntersections/number-of-disc-intersections.go
@@ -1,16 +1,42 @@
 //Compute the number of intersections in a sequence of discs.
 //https://codility.com/programmers/lessons/6-sorting/number_of_disc_intersections/
+/*
+Given array indices i, j (i < j)
+If the discs are located at (0,i) and (0,j) respectively
+Then the discs will intersect if j − i <= A[i] + A[j]
+*/
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	A := []int{1, 5, 2, 1, 4, 0}
-	Output := Solution(A)
-	fmt.Printf("A=%v\nOutput=%v\n", A, Output)
+	Num := Solution(A)
+	fmt.Printf("A=%v\nNum=%v\n", A, Num)
 }
 
 func Solution(A []int) int {
+	upper := make([]int, len(A))
+	lower := make([]int, len(A))
+	for i := 0; i < len(A); i++ {
+		upper[i] = A[i] + i
+		lower[i] = -(A[i] - i)
+	}
+	sort.Ints(upper)
+	sort.Ints(lower)
 
-	return 0
+	ret := 0
+	i, j := 0, 1
+	for ; i < len(A); i++ {
+		if ret > 10000000 {
+			return -1
+		}
+		for ; j < len(A) && upper[i] >= lower[j]; j++ {
+			ret += j - i
+		}
+	}
+	return ret
 }
